d4b2b1e485dfb3b59bde837b44cdb8181a49301e
Vinit Surve <115409878+VinitSurve@users.noreply.github.com>
2024-07-20 12:38:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function filterElements() {
+    const input = document.getElementById('searchInput').value.toUpperCase();
+    const container = document.getElementById('elementContainer');
+    const cards = container.getElementsByClassName('card');
+
+    for (let i = 0; i < cards.length; i++) {
+        const elementName = cards[i].getElementsByClassName('element-name')[0];
+        const elementSymbol = cards[i].getElementsByClassName('element-symbol')[0];
+        const elementNumber = cards[i].getElementsByClassName('element-number')[0];
+
+        if (elementName.innerHTML.toUpperCase().indexOf(input) > -1 ||
+            elementSymbol.innerHTML.toUpperCase().indexOf(input) > -1 ||
+            elementNumber.innerHTML.indexOf(input) > -1) {
+            cards[i].parentElement.style.display = "";
+        } else {
+            cards[i].parentElement.style.display = "none";
+        }
+    }
+}
